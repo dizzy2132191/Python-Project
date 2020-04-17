62ea821a917852cd59f1048dd885c3a4685e147c
dizzy2132191 <dizzy2132191@naver.com>
2020-04-17 15:12:01 +0900

    처음 올려봄

---
+++ test.py
@@ -0,0 +1,31 @@
+'''
+season='겨울'
+print('지금은','이 따옴표로 쌓인 글을 지우고 대신 변수 season을 넣어 보세요','입니다.')
+#이 프린트 문은 season 변수를 읽어 냅니다.
+print('지금은','이 따옴표로 쌓인 글을 지우고 대신 변수', season,'을 넣어 보세요','입니다.')
+
+#'', "" ==> 문자열
+# 안둘러 싸져 있음? ==> 변수
+#ㅁㄴㅇㅎㅁㄴㅇㅎㅁㅈㄷㅎㅁㅎㄴㅇㅎㅋㅌㅎㅋㄴ
+#print('지금은','이 따옴표로 쌓인 글을 지우고 대신 변수', season,'을 넣어 보세요','입니다.')
+
+print('지금은','이 따옴표로 쌓인 글을 지우고 대신 변수', season,'을 넣어 보세요','입니다.')
+print('지금은','이 따옴표로 쌓인 글을 지우고 대신 변수', season,'을 넣어 보세요','입니다.')
+'''
+
+#///////////////////////////////////////////////////////////////////////////////////////
+
+# 1 , '1'
+a = 33
+b = 3
+summation = a + b
+multiply = a * b
+divide = a / b
+remainder = a % b
+power = a ** b
+
+print("summation은 {}입니다.".format(summation))
+print("multiply는 {}입니다.".format(multiply))
+print("divide는 {}입니다.".format(divide))
+print("remainder는 {}입니다.".format(remainder))
+print("power는 {}입니다.".format(power))
